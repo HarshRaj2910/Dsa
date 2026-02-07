21f59e969e6f2740d15f19d6c50759aa490fbdd2
HarshRaj2910 <harshroman617@gmail.com>
2026-02-07 11:30:35 +0530

    b

--- Array/hello.java
+++ Array/hello.java
@@ -4,7 +4,7 @@ package Array;
 import java.util.*;
 public class hello {
 
-    // print 100 hello
+    // 1. print 100 hello
 
 //  public static void main(String[] args) {
 //         int counter=0;
@@ -17,10 +17,10 @@ public class hello {
     
 // }
 
-        // print number from 1 to n
+        // 2. print number from 1 to n
 
          public static void main(String[] args) {
-    //     Scanner sc=new Scanner(System.in);
+         Scanner sc=new Scanner(System.in);
     //     System.out.println("print the number upto you want to print: ");
     //     int n=sc.nextInt();
     //     System.out.println("print the starting number: ");
@@ -31,9 +31,9 @@ public class hello {
     //     }
     // }
 
-    // print sum of first n natural number
+    // 3. print sum of first n natural number
     
-    Scanner sc= new Scanner(System.in);
+ //   Scanner sc= new Scanner(System.in);
 //     System.out.println("print last number: ");
 //     int n=sc.nextInt();
 //     int sum=0; // initial sum is zero
@@ -47,16 +47,58 @@ public class hello {
 
 // }
 
-        // print reverse of a number
-        System.out.println("print the number: ");
-        int num=sc.nextInt();
-        while(num > 0){
-            int lastDigit=num%10;  //  find last digit 
-            System.out.print(lastDigit+" ");
-            num/=10; // remove the last digit
+        // 4. print reverse of a number
+
+        // System.out.println("print the number: ");
+        // int num=sc.nextInt();
+        // while(num > 0){
+        //     int lastDigit=num%10;  //  find last digit 
+        //     System.out.print(lastDigit+" ");
+        //     num/=10; // remove the last digit
+        // }
+
+        // 5. print reverse of a given number
+//         System.out.println("user give a number: ");
+//         int num=sc.nextInt();
+//         int rev=0;
+//         while(num > 0){
+//             int lastDigit=num % 10; // check last digit
+//             rev=(rev*10)+lastDigit;
+//             num/=10;
+//         }
+//         System.out.println(rev);
+
+
+// }
+
+            // 6. enter multiple of 10 using break statement
+            do{
+                System.out.println("enter your number: ");
+                int n=sc.nextInt();
+                if(n % 10 == 0){
+                    break;  // to exit the loop when condition become true
+                }
+                System.out.println(n);
+            }while(true);
+
+            // 7. enter multiple of 7 using continue statement
+            do {
+    System.out.println("Enter your number: ");
+    int n = sc.nextInt();
+
+    if (n == 0) {
+        break;  // stop the loop
+    }
+
+    if (n % 7 == 0) {
+        continue;   // skip the iteration
+    }
+
+    System.out.println("Number was: " + n);
+
+} while (true);
+
         }
 
-
-}
 }
 
