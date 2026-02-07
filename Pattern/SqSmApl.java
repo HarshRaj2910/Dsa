7bde2ab50f8b6d892fa3702b47e5617e35ac13e5
HarshRaj2910 <harshroman617@gmail.com>
2026-02-07 15:19:41 +0530

    e

--- Pattern/SqSmApl.java
+++ Pattern/SqSmApl.java
@@ -1,12 +1,12 @@
 package Pattern;
 import java.util.*;
-public class SqApl {
+public class SqSmApl {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                System.out.print((char)(j+64)+" ");   // type cast, Capital letter print 64 is starting number
+                System.out.print((char)(j+96)+" ");
             }
             System.out.println();
         }
